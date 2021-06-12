c9ea3eb1394c612a352c41e312c6a563525131e4
Wong Yun Rui Chris <wongyunruichris@gmail.com>
2021-06-12 22:43:11 +0800

    merged

--- component/todoList.js
+++ component/todoList.js
@@ -42,7 +42,6 @@ export default function todoList( { type, navigation} ) {
                       {timeToHourMin(dateTimeMerge(item.endDate, item.endTime))}
                     </Paragraph>
                       : <Paragraph>{item.duration}</Paragraph>}
-                  <Title>{item.name}</Title>
                 </Card.Content>
               </Card>
             </View>
