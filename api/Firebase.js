e4d6ab7586137ac103669d999302d56d8f8dd9cf
Wong Yun Rui Chris <wongyunruichris@gmail.com>
2021-07-03 21:53:13 +0800

    update api key

--- api/Firebase.js
+++ api/Firebase.js
@@ -1,17 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/auth"
 import "firebase/database"
-import {GOOGLE_API_KEY} from "@env"
+import {apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId, measurementId} from "@env"
 
 const firebaseConfig = {
-  apiKey: GOOGLE_API_KEY,
-  authDomain: "whyboarder-fc081.firebaseapp.com",
-  databaseURL: "https://whyboarder-fc081-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "whyboarder-fc081",
-  storageBucket: "whyboarder-fc081.appspot.com",
-  messagingSenderId: "548391206823",
-  appId: "1:548391206823:web:517d61a42cc09f57148082",
-  measurementId: "G-T5HEGL3XG0"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 };
 
 const firebaseApp = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
